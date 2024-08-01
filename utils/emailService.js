738d970eebcd6c5aa60bcb45a5e08d14a1b81b1f
Muhammad Shoaib <sendmailtoshoaib@gmail.com>
2024-08-02 04:56:19 +0500

    changes

--- utils/emailService.js
+++ utils/emailService.js
@@ -2,12 +2,12 @@ const nodemailer = require('nodemailer');
 
 // Configure the transporter
 const transporter = nodemailer.createTransport({
-  host: '', // SMTP Host
+  host: 'smtp.hostinger.com', // SMTP Host
   port: 465, // SMTP Port (commonly 587 for secure connection)
-  secure: false, // true for 465, false for other ports
+  secure: true, // true for 465, false for other ports
   auth: {
-    user: '', // SMTP username
-    pass: ' // SMTP password
+    user: 'otpverification@myredd.net', // SMTP username
+    pass: 'otp@Riyadh12' , // SMTP password
   },
   tls: {
     rejectUnauthorized: false // Only use this option during development to bypass certain TLS restrictions.
